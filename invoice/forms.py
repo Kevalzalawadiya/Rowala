2aa9b8e482dc3133e4d0fb98558a4e40741a8b6e
Ubuntu <ubuntu@ip-172-31-16-231.eu-north-1.compute.internal>
2023-12-22 14:38:39 +0000

    Update Speling misteck.

--- invoice/forms.py
+++ invoice/forms.py
@@ -48,7 +48,7 @@ class InvoiceForm(forms.ModelForm):
             'customer': forms.TextInput(attrs={
                 'class': 'form-control',
                 'id': 'invoice_customer',
-                'placeholder': 'Jane Deo',
+                'placeholder': 'Name',
             }),
             'contact': forms.TextInput(attrs={
                 'class': 'form-control',
